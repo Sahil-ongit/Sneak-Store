16dfac483ddd17445c33601b7c2a7d8b90d4acc6
Darker621 <sahil.sajayan63@gmail.com>
2023-11-11 21:50:08 +0530

    changed

---
+++ lib/samplex.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SamplePage extends StatefulWidget {
+  const SamplePage({super.key});
+
+  @override
+  State<SamplePage> createState() => _SamplePageState();
+}
+
+class _SamplePageState extends State<SamplePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
